4952ae1313be68ff7822af65cea9d9c2b302c99c
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-03-09 04:05:26 +0530

    Update CreateListing.tsx
    both camera and file

--- src/pages/CreateListing.tsx
+++ src/pages/CreateListing.tsx
@@ -467,7 +467,6 @@ const onSubmit = async (values: z.infer<typeof listingFormSchema>) => {
                             <Input
                               type="file"
                               accept="image/*"
-                              capture="user"
                               multiple
                               onChange={handleImageUpload}
                               className="hidden"
